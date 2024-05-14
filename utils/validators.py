d6f37deeede259de48e5e8be1d5d58d23634759f
levmerkin124 <chipstop2505@gmail.com>
2024-05-14 20:23:30 +0300

    add pytest functionality

---
+++ utils/validators.py
@@ -0,0 +1,10 @@
+def is_valid_email(email_candidate: str) -> bool:
+    if email_candidate.count("@") > 1:
+        return False
+
+    email_parts = email_candidate.split("@")
+    if len(email_parts) == 2:
+        return True
+    return False
+
+is_valid_email('ergre')
