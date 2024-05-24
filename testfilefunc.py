f2d9944e70a584c0e850896764bf7f50341bbf53
levmerkin124 <chipstop2505@gmail.com>
2024-05-24 19:12:41 +0300

    add pytest functionality

--- testfilefunc.py
+++ testfilefunc.py
@@ -1,18 +1,18 @@
-def longer_string(s1: str, s2: str) -> str:
+def test_longer_string(s1: str, s2: str) -> str:
     """
     Повертає довшу з двох строк.
     """
     return s1 if len(s1) >= len(s2) else s2
 
 
-def numeric_list(lst: list) -> bool:
+def test_numeric_list(lst: list) -> bool:
     """
     Перевіряє чи всі елементи у списку є числами.
     """
     return all(isinstance(item, (int, float)) for item in lst)
 
 
-def print_line() -> None:
+def test_print_line() -> None:
     """
     Виводить строку з 80 зірочок.
     """
