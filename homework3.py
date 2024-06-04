3fd278a06799d344dbab5fa083f42a540267f2db
levmerkin124 <chipstop2505@gmail.com>
2024-06-04 19:04:27 +0300

    add pytest functionality

--- homework3.py
+++ homework3.py
@@ -1,18 +1,18 @@
-def test_longer_string(s1: str, s2: str) -> str:
+def longer_string(s1: str, s2: str) -> str:
     """
     Повертає довшу з двох строк.
     """
     return s1 if len(s1) >= len(s2) else s2
 
 
-def test_numeric_list(lst: list) -> bool:
+def numeric_list(lst: list) -> bool:
     """
     Перевіряє чи всі елементи у списку є числами.
     """
     return all(isinstance(item, (int, float)) for item in lst)
 
 
-def test_print_line() -> None:
+def print_line() -> None:
     """
     Виводить строку з 80 зірочок.
     """
